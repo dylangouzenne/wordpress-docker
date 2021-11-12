4c387ac94e97d2f39f0b7159f3bae1290bfb1305
dylangouzenne <dylan.gouzenne@gmail.com>
2021-11-12 17:29:42 +0100

    fix install script wp-cli

--- docker_images/srv_php/install_wp-cli.sh
+++ docker_images/srv_php/install_wp-cli.sh
@@ -22,7 +22,7 @@ wp core config --dbname=${MYSQL_DATABASE} --dbuser=${MYSQL_USER} --dbpass=${MYSQ
 EOF
 
 # Création de la base de donnée avec les paramètre du fichier wp-config.php
-wp db create
+#wp db create
 
 # Réalisation de l'instalation du site : définition de l'url d'accès, du titre du site et pour finir création du compte admin
 wp core install --url=${WORDPRESS_URL} --title=${WORDPRESS_TITLE} --admin_user=${WORDPRESS_ADMIN_USER} --admin_password=${WORDPRESS_ADMIN_PASSWORD} --admin_email=${WORDPRESS_ADMIN_EMAIL}
